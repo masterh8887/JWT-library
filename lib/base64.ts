ea5080b8eb3a45b3cc089bd977c8c008c7d44a18
Master-Hun <masterh8887@gmail.com>
2020-04-28 16:43:09 +0900

    makingJWT(#1)

---
+++ lib/base64.ts
@@ -0,0 +1,46 @@
+"use strict";
+
+export function encode(str: string) {
+  console.log(encode.prototype);
+  if (
+    Buffer.byteLength(str) !== str.length ||
+    Buffer.byteLength(str) === 0 ||
+    str.length === 0
+  )
+    throw new Error("bad string or empty string data");
+
+  return Buffer.from(str, "binary").toString("base64");
+}
+
+export function toUrlEncode(str: string) {
+  if (
+    Buffer.byteLength(str) !== str.length ||
+    Buffer.byteLength(str) === 0 ||
+    str.length === 0
+  )
+    throw new Error("bad string or empty string data");
+
+  return str.replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
+}
+
+export function decode(str: string) {
+  if (
+    Buffer.byteLength(str) !== str.length ||
+    Buffer.byteLength(str) === 0 ||
+    str.length === 0
+  )
+    throw new Error("bad string or empty string data");
+
+  return Buffer.from(str, "base64").toString();
+}
+
+export function toBase64(str: string) {
+  if (
+    Buffer.byteLength(str) !== str.length ||
+    Buffer.byteLength(str) === 0 ||
+    str.length === 0
+  )
+    throw new Error("bad string or empty string data");
+
+  return str.toString().replace(/\-/g, "+").replace(/_/g, "/");
+}
